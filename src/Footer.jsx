f7dfafe222c95b699c8d9cceed0f37ce1cb3ac6a
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-20 19:43:40 +0100

    Add the Footer component

---
+++ src/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+	return (
+		<div className="gitHubLinkContainer">
+			<a
+				href="https://github.com/Haminimi"
+				className="gitHubLink"
+				aria-label="GitHub"
+			>
+				<i className="devicon-github-original"></i>
+			</a>
+		</div>
+	);
+}
+
+export default Footer;
