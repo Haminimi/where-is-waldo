0fd4a24a07e4c0bc42f9fa2dbe57b6a63b7672d4
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-20 21:34:01 +0100

    Add the Header component

---
+++ src/Header.jsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import waldo from './assets/images/waldo.webp';
+import sound from '../utility/sound';
+import buttonSound from './assets/sounds/button-sound.wav';
+
+function Header({ currentPath, music, setMusic }) {
+	return (
+		<>
+			<header>
+				<img src={waldo} alt="" className="headerImage" />
+				<nav className="headerNav">
+					<Link
+						to="/"
+						className={
+							currentPath === '/' ? 'activeLink' : 'inactiveLink'
+						}
+						onClick={() => sound(buttonSound)}
+					>
+						Home
+					</Link>{' '}
+					<Link
+						to="/leaderboard"
+						className={
+							currentPath === '/leaderboard'
+								? 'activeLink'
+								: 'inactiveLink'
+						}
+						onClick={() => sound(buttonSound)}
+					>
+						Leaderboard
+					</Link>
+					{music === 'on' ? (
+						<span
+							className="material-symbols-outlined noteIcon"
+							onClick={() => setMusic('off')}
+						>
+							music_note
+						</span>
+					) : (
+						<span
+							className="material-symbols-outlined noteIcon"
+							onClick={() => setMusic('on')}
+						>
+							music_off
+						</span>
+					)}
+				</nav>
+			</header>
+		</>
+	);
+}
+
+export default Header;
