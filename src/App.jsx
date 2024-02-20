672bb0a5a8dada2d9c0e8e4cd978508c07b8bc0f
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-20 20:42:11 +0100

    Add the background music

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
 import Game from './Game';
 import Leaderboard from './Leaderboard';
 import Footer from './Footer';
+import backgroundMusic from './assets/background-music.mp3';
 
 function App() {
 	const [currentImage, setCurrentImage] = useState('beach');
+	const [music, setMusic] = useState('off');
 	const { name } = useParams();
 	const location = useLocation();
 	const currentPath = location.pathname;
 
+	useEffect(() => {
+		const audio = new Audio(backgroundMusic);
+		const handleEnded = () => {
+			audio.currentTime = 0;
+			audio.play();
+		};
+
+		if (music === 'on') {
+			audio.loop = true;
+
+			audio.addEventListener('ended', handleEnded);
+
+			audio.play();
+		} else {
+			audio.pause();
+		}
+
+		return () => {
+			audio.removeEventListener('ended', handleEnded);
+			audio.pause();
+		};
+	}, [music]);
+
 	return (
 		<>
-			<Header currentPath={currentPath} />
+			<Header
+				currentPath={currentPath}
+				music={music}
+				setMusic={setMusic}
+			/>
 			{name === 'game' ? (
 				<Game currentImage={currentImage} />
 			) : name === 'leaderboard' ? (
